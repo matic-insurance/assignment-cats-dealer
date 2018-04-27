dc1c6613d9b049d2273bc4538b07a334833fa6f3
pasternak <pasternak@fidor.com>
2018-04-27 22:21:10 +0300

    Add StubHelper for specs

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe 'I18n' do
   let(:unused_keys) { i18n.unused_keys }
 
   it 'does not have missing keys' do
-    expect(missing_keys).to be_empty, "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    message = "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+    expect(missing_keys).to be_empty, message
   end
 
   it 'does not have unused keys' do
-    expect(unused_keys).to be_empty, "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    message = "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+    expect(unused_keys).to be_empty, message
   end
 
   it 'files are normalized' do
