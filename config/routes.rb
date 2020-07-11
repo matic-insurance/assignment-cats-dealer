a1a7e3c7bd473fc41765eaee923c40ce953521f2
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-11 19:10:00 +0300

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cats#index'
 
-  resource :request, only: %i[create new] do
-    get :result
-  end
-
-
   post :cats, to: 'cats#search'
   resources :cats, only: %i[new index]
   
