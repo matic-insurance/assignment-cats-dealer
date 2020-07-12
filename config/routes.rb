04abaea83afdda7062a4c558a4c4db697936ee45
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-13 00:05:42 +0300

    use get instead of post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cats#index'
 
-  post :cats, to: 'cats#search'
+  get :search, to: 'cats#search'
   resources :cats, only: %i[index]
   
 end
