15312dba756a4710371c7073833124954fa261c1
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-12 17:13:31 +0300

    refactor

--- app/helpers/cats/cat_helper.rb
+++ app/helpers/cats/cat_helper.rb
@@ -1,11 +1,13 @@
 module Cats
   module CatHelper
     def cat_breeds
-      %w(Abyssin American\ Curl Bengal Bobtail British\ Shorthair Devon\ Rex Grumpy Maine\ Coon Persian Siamese Sphynx).map { |breed| [breed, breed] }
+      %w[Abyssin American\ Curl Bengal Bobtail
+         British\ Shorthair Devon\ Rex Grumpy Maine\ Coon
+         Persian Siamese Sphynx].map { |breed| [breed, breed] }
     end
 
     def cities
-      %w(Kharkiv Kyiv Lviv Odessa).map { |city| [city, city] }
+      %w[Kharkiv Kyiv Lviv Odessa].map { |city| [city, city] }
     end
   end
 end
