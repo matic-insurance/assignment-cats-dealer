1a02918084d615f31a20730c451d1d7fc4499259
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-12 12:49:47 +0300

    view helpers

--- app/helpers/cats/cat_helper.rb
+++ app/helpers/cats/cat_helper.rb
@@ -1,4 +1,11 @@
 module Cats
   module CatHelper
+    def cat_breeds
+      %w(Abyssin American\ Curl Bengal Bobtail British\ Shorthair Devon\ Rex Grumpy Maine\ Coon Persian Siamese Sphynx).map { |breed| [breed, breed] }
+    end
+
+    def cities
+      %w(Kharkiv Kyiv Lviv Odessa).map { |city| [city, city] }
+    end
   end
 end
