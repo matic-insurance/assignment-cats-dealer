9e6c5f3de44a439cfae79c7c1d447af2344f3197
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-13 11:50:32 +0300

    cleanup

--- spec/services/cat_providers/cats_unlimited_spec.rb
+++ spec/services/cat_providers/cats_unlimited_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe CatsProviders::CatsUnlimited do
-  subject { described_class.new(SearchRequest.new(breed: 'Bengal', user_location: 'Kyiv')) }
+  subject(:provider) { described_class.new(SearchRequest.new(breed: 'Bengal', user_location: 'Kyiv')) }
 
   it { is_expected.to respond_to(:fetch_deals) }
   it { is_expected.to respond_to(:deals) }
 
   describe '#fetch_deals' do
-    specify { expect { subject.fetch_deals }.to change { subject.deals.present? }.to(true) }
-    specify { expect { subject.fetch_deals }.to change { subject.deals.first }.to(hash_including('name' => 'Bengal', 'location' => 'Kyiv')) }
+    specify { expect { provider.fetch_deals }.to change { provider.deals.present? }.to(true) }
+    specify { expect { provider.fetch_deals }.to change { provider.deals.first }.to(hash_including('name' => 'Bengal', 'location' => 'Kyiv')) }
   end
-
 end
