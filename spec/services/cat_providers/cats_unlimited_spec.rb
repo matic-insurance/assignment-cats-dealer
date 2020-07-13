99e28f4429e6bcfc9d13b113b269102e9c9a3538
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-13 10:07:59 +0300

    cleanup

--- spec/services/cat_providers/cats_unlimited_spec.rb
+++ spec/services/cat_providers/cats_unlimited_spec.rb
@@ -6,7 +6,7 @@ describe CatsProviders::CatsUnlimited do
   it { is_expected.to respond_to(:fetch_deals) }
   it { is_expected.to respond_to(:deals) }
 
-  describe "#fetch_deals" do
+  describe '#fetch_deals' do
     specify { expect { subject.fetch_deals }.to change { subject.deals.present? }.to(true) }
     specify { expect { subject.fetch_deals }.to change { subject.deals.first }.to(hash_including('name' => 'Bengal', 'location' => 'Kyiv')) }
   end
