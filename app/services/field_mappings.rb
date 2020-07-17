2a85cda36507b13add953867b10507a1c46119ce
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-17 17:54:53 +0300

    extend FieldMapper

--- app/services/field_mappings.rb
+++ app/services/field_mappings.rb
@@ -10,4 +10,18 @@ module FieldMapings
       location: :location
     }
   end
+
+  def self.apply_field_mappings!(deals)
+    deals.each_with_object([]) do |deal, deals|
+      deals << build_deal(deal)
+      deals
+    end
+  end
+
+  def self.build_deal(current_deal)
+  current_deal.keys.each_with_object({}) do |deal_key, deal|
+    deal[field_name_mappings[deal_key.to_sym]] = current_deal[deal_key]
+    deal
+  end
+  end
 end
