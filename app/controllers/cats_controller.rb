46d89a806bed2260b139010dcd002ebcca596f53
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-22 00:15:56 +0300

    cleanup

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,18 +2,11 @@ class CatsController < ApplicationController
   def search
     @cats = CatsDealer.search(SearchRequest.new(search_params))
 
-    respond_to do |format|
-      format.html
-      format.json { render json: @cats }
-    end
+    @user_location = params.fetch('user_location', nil)
+    @breed = params.fetch('breed', nil)
+
+    render 'index'
   end
-  #
-  # def index
-  #   @cats = params.fetch('cats', [])
-  #   @breed = params.fetch('breed', nil)
-  #   @user_location = params.fetch('user_location', nil)
-  #   @searched = params.fetch('searched', nil)
-  # end
 
   private
 
