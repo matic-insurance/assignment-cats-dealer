ad81eff66ac20a2cf750aa5ad59c49c506b49b4c
Oleh Adam Dubnytskyy <adam.dubnytskyy@gmail.com>
2020-07-22 00:15:25 +0300

    cleanup

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,7 +1,3 @@
 class Cat < ApplicationRecord
   belongs_to :provider, optional: true
-
-  def self.pussies(provider_name)
-    where(provider_id: Provider.find_by(name: provider_name))add
-  end
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -3,6 +3,8 @@ class Provider < ApplicationRecord
   has_and_belongs_to_many :locations
 
   def self.provider_deals(provider_name)
-    select(:'cats.id', :breed, :price, :location, :image_src).joins(:cats).where(name: provider_name)
+    Cat.joins("INNER JOIN providers ON providers.id = cats.provider_id")
+        .select(:breed, :price, :location, :image_src)
+        .where(provider_id: find_by(name: provider_name))
   end
 end
