99cccb675e9ae7d9a73803cfe2b996cdae0d3776
Rostyslav Safonov <elhowm@gmail.com>
2021-06-27 23:48:39 +0300

    Add caching to Cats::Fetch

--- app/interactors/cats/fetch.rb
+++ app/interactors/cats/fetch.rb
@@ -8,17 +8,24 @@ module Cats
       ::Cats::Providers::CatsUnlimitedService,
       ::Cats::Providers::HappyCatsService
     ].freeze
+    CACHE_TIME = 12.hours
 
     delegate :filters, to: :context
 
     def call
-      all = PROVIDERS.map { |provider| provider.instance.data }.flatten
-      context.list = filter_cats(all)
+      context.list = result
     rescue ProviderUnavailableError => e
       context.fail!(error: e.message)
     end
 
     private
+
+    def result
+      all = ::Rails.cache.fetch('cats/fetch', expires_in: CACHE_TIME) do
+        PROVIDERS.map { |provider| provider.instance.data }.flatten
+      end
+      filter_cats(all)
+    end
 
     def filter_cats(list)
       return list if filters.blank?
