b9a8a7e711ce9c9b4c8649b82bfe3989f327175d
Rostyslav Safonov <elhowm@gmail.com>
2021-06-27 22:50:20 +0300

    Upgrade to ruby 3.0.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 
-Dir["#{::Rails.root}/spec/support/**/*.rb"].sort.each { |helper| require helper }
+Dir["#{::Rails.root}/spec/support/**/*.rb"].each { |helper| require helper }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
