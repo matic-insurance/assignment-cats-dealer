cae02c1dd1980ca841ffef9df5e596bf356dd5ae
Anna <akhsunna4@gmail.com>
2021-11-29 13:21:34 +0200

    Make CatList#where not case sensitive

--- app/models/shops/cat_list.rb
+++ app/models/shops/cat_list.rb
@@ -4,7 +4,9 @@ module Shops
   class CatList < Array
     def where(params)
       CatList.new(
-        select { |c| c.to_h.slice(*params.keys) == params }
+        select do |c|
+          c.to_h.values_at(*params.keys).map(&:downcase) == params.values.map(&:downcase)
+        end
       )
     end
   end
--- spec/models/shops/cat_list_spec.rb
+++ spec/models/shops/cat_list_spec.rb
@@ -10,10 +10,18 @@ describe Shops::CatList do
   let(:cat_list) { described_class.new([cat1, cat2, cat3]) }
 
   describe '#where' do
-    it 'returns filtered cats' do
-      result = cat_list.where(cat_type: 'Bengal', location: 'Odessa')
+    let(:result) { cat_list.where(cat_type: 'Bengal', location: 'Odessa') }
 
+    it 'returns filtered cats' do
       expect(result).to eq(described_class.new([cat2]))
+    end
+
+    context 'when filter values with lowercase letter' do
+      let(:result) { cat_list.where(cat_type: 'bengal', location: 'odessa') }
+
+      it 'works the same: returns filtered cats' do
+        expect(result).to eq(described_class.new([cat2]))
+      end
     end
   end
 end
